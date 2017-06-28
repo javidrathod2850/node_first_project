59fa4a419de1e5d1e612f231418d3c6eaa8fdfb3
Javid <javid@OBLAP-46>
2017-06-28 16:50:27 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express    =    require('express');
+var session = require('express-session');
+var app        =    express();
+var sess;
+app.use(session({secret: 'ssshhhhh'}));
+
+require('./router/main')(app);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static('views'))
+app.engine('html', require('ejs').renderFile);
+
+
+//     app.get('/',function(req,res){
+//     res.sendfile('index.html');
+// });
+
+var server     =    app.listen(3000,function(){
+    console.log("We have started our server on port 3000");
+});
